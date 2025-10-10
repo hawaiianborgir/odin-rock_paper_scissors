0cd96c467af9a55e58ebe23ae379830248fa68d9
hawaiianborgir <shinkyu49@gmail.com>
2025-10-10 20:36:15 +0900

    Add playGame function to play the game 5 times

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -69,12 +69,23 @@ function playRound(humanChoice, compChoice) {
         humanScore += 1;
         console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
     }
-    return (humanScore, compScore)
+    return (humanScore, compScore);
 }
 
-const humanSelection = getHumanChoice()
-const compSelection = getComputerChoice()
+let humanSelection;
+let compSelection;
 
-playRound(humanSelection, compSelection)
+function playGame() {
+    for (let i=1; i<=5; i++) {
+        humanSelection = getHumanChoice();
+        compSelection = getComputerChoice();
+        playRound(humanSelection, compSelection);
+    }
+}
 
-// is it okay to commit one big change (not micro changes) in one commit?
+playGame()
+
+
+
+// this game has no countermeasure to address typo by user.
+// I can add it, but it is not the focus for this project.
