248c5720a73479f6d9686350f66aca66d50d16fd
hawaiianborgir <shinkyu49@gmail.com>
2025-10-10 20:03:51 +0900

    Add complete logic for play round function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -23,6 +23,7 @@ humanScore = 0
 compScore = 0
 
 function playRound(humanChoice, compChoice) {
+    // human rock
     if (humanChoice == "rock" && compChoice == "rock") {
         console.log("Player:rock vs. NPC:rock");
         console.log("DRAW!");
@@ -34,6 +35,36 @@ function playRound(humanChoice, compChoice) {
         console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
     } else if (humanChoice == "rock" && compChoice == "scissors") {
         console.log("Player:rock vs. NPC:scissors");
+        console.log("You WIN!");
+        humanScore += 1;
+        console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
+    // human paper
+    } else if (humanChoice == "paper" && compChoice == "paper") {
+        console.log("Player:paper vs. NPC:paper");
+        console.log("DRAW!");
+        console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
+    } else if (humanChoice == "paper" && compChoice == "scissors") {
+        console.log("Player:paper vs. NPC:scissors");
+        console.log("You Lose");
+        compScore += 1;
+        console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
+    } else if (humanChoice == "paper" && compChoice == "rock") {
+        console.log("Player:paper vs. NPC:rock");
+        console.log("You WIN!");
+        humanScore += 1;
+        console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
+    // human scissors
+    } else if (humanChoice == "scissors" && compChoice == "scissors") {
+        console.log("Player:scissors vs. NPC:scissors");
+        console.log("DRAW!");
+        console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
+    } else if (humanChoice == "scissors" && compChoice == "rock") {
+        console.log("Player:scissors vs. NPC:rock");
+        console.log("You Lose");
+        compScore += 1;
+        console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
+    } else if (humanChoice == "scissors" && compChoice == "paper") {
+        console.log("Player:scissors vs. NPC:paper");
         console.log("You WIN!");
         humanScore += 1;
         console.log("Player Score: " + humanScore +" & " +"NPC Score: " +compScore);
